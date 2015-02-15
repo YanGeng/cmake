1abc2981479a9dd48d333812fe87e7b7556bdea5
YanGeng <18516054923@163.com>
2015-02-16 00:01:23 +0800

    Add the multi test for cmake

---
+++ multi_dir/hello/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+void Hello::sayHello() {
+	cout << "Hello!\n" << endl;
+}
---
+++ multi_dir/hello/hello.h
@@ -0,0 +1,10 @@
+#ifndef __HELLO_H
+#define __HELLO_H
+
+class Hello
+{
+	public:
+		void sayHello();
+};
+
+#endif
